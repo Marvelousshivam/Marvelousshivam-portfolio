50f11043f24d9c860949c26016ba71346aab7fdc
Marvelousshivam <shivamrajuniverse616@gmail.com>
2026-07-09 01:48:37 +0530

    Disable Next.js outputFileTracing to prevent ENOENT build errors on Windows

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracing: false,
   images: {
     domains: ['i.scdn.co', 'c.saavncdn.com', 'i.ytimg.com', 'lh3.googleusercontent.com', 'yt3.ggpht.com', 'yt3.googleusercontent.com'],
   },
